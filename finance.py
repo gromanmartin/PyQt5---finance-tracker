8ae222cfdd4fe58329474be0639df17da90fdb24
learninghowtocode9 <learninghowtocode9@gmail.com>
2020-04-12 16:31:25 -0600

    Made a simple menu window

--- finance.py
+++ finance.py
@@ -1,20 +1,34 @@
-import tkinter as tk
+import tkinter as tk 
+from tkinter import ttk
 
-class Buttons:
-
-	def __init__(self, master):
-		frame = tk.Frame(master)
-		frame.pack()
-
-		self.printButton = tk.Button(frame, text='Print message', command=self.printMessage)
-		self.printButton.pack(side=tk.LEFT)
-
-		self.quitButton = tk.Button(frame, text='Quit', command=frame.quit)
-		self.quitButton.pack(side=tk.LEFT)
-
-	def printMessage(self):
-		print('WOOOOOOOOOOOOOOOOW')
 
 root = tk.Tk()
-b = Buttons(root)
+root.geometry("750x400")
+root.grid_rowconfigure(0, weight=1)
+root.grid_columnconfigure(0, weight=1)
+
+mainFrame = tk.Frame(root)
+mainFrame.grid(row=0, column=0, sticky='NSEW')
+mainFrame.grid_rowconfigure(0, weight=1)
+mainFrame.grid_columnconfigure(0, weight=1)
+
+label1 = tk.Label(mainFrame, text='Welcome to your finance tracker', font=('Font',32))
+label1.grid()
+
+menuFrame = tk.Frame(root)
+menuFrame.grid(row=1, column=0, sticky='NSEW', pady=50)
+menuFrame.grid_rowconfigure(1, weight=1)
+menuFrame.grid_columnconfigure(0, weight=1)
+
+buttonStyle = ttk.Style()
+buttonStyle.configure('A.TButton', width = 20, font=('',24))
+button_login = ttk.Button(menuFrame, text='Login', style='A.TButton')
+button_newacc = ttk.Button(menuFrame, text='Create new account', style='A.TButton')
+button_exit = ttk.Button(menuFrame, text='Exit', style='A.TButton')
+
+button_login.grid()
+button_newacc.grid()
+button_exit.grid()
+
+
 root.mainloop()
