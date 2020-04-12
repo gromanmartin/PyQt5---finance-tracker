c6653d1b4aed0c2a6a5a55b1f72f83a04cd18226
learninghowtocode9 <learninghowtocode9@gmail.com>
2020-04-12 13:03:36 -0600

    Initial commit

---
+++ finance.py
@@ -0,0 +1,21 @@
+import tkinter as tk
+
+class Buttons:
+
+	def __init__(self, master):
+		frame = tk.Frame(master)
+		frame.pack()
+
+		self.printButton = tk.Button(frame, text='Print message', command=self.printMessage)
+		self.printButton.pack(side=tk.LEFT)
+
+		self.quitButton = tk.Button(frame, text='Quit', command=frame.quit)
+		self.quitButton.pack(side=tk.LEFT)
+
+	def printMessage(self):
+		print('WOOOOOOOOOOOOOOOOW')
+
+
+root = tk.Tk()
+b = Buttons(root)
+root.mainloop()
