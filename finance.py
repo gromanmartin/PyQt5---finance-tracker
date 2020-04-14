81e2b63c0ac7244b3272f5b609263a2bffd35846
learninghowtocode9 <learninghowtocode9@gmail.com>
2020-04-13 23:42:26 -0600

    Phase one - buttons working

--- finance.py
+++ finance.py
@@ -1,34 +1,111 @@
-import tkinter as tk 
-from tkinter import ttk
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+import sys
 
 
-root = tk.Tk()
-root.geometry("750x400")
-root.grid_rowconfigure(0, weight=1)
-root.grid_columnconfigure(0, weight=1)
+class ApplicationWindow(QWidget):
 
-mainFrame = tk.Frame(root)
-mainFrame.grid(row=0, column=0, sticky='NSEW')
-mainFrame.grid_rowconfigure(0, weight=1)
-mainFrame.grid_columnconfigure(0, weight=1)
+    def __init__(self):
+        super(ApplicationWindow, self).__init__()
+        self.menu = MenuWindow()
+        self.login = LoginWindow()
+        self.createacc = CreateAccWindow()
 
-label1 = tk.Label(mainFrame, text='Welcome to your finance tracker', font=('Font',32))
-label1.grid()
+        self.setGeometry(0, 0, 800, 600)
+        self.setWindowTitle('Finance tracker')
 
-menuFrame = tk.Frame(root)
-menuFrame.grid(row=1, column=0, sticky='NSEW', pady=50)
-menuFrame.grid_rowconfigure(1, weight=1)
-menuFrame.grid_columnconfigure(0, weight=1)
+        self.setAutoFillBackground(True)
+        p = self.palette()
+        p.setColor(self.backgroundRole(), Qt.green)
+        self.setPalette(p)
 
-buttonStyle = ttk.Style()
-buttonStyle.configure('A.TButton', width = 20, font=('',24))
-button_login = ttk.Button(menuFrame, text='Login', style='A.TButton')
-button_newacc = ttk.Button(menuFrame, text='Create new account', style='A.TButton')
-button_exit = ttk.Button(menuFrame, text='Exit', style='A.TButton')
+        self.stack = QStackedWidget()
+        self.stack.setSizePolicy(QSizePolicy.Maximum, QSizePolicy.Maximum)
+        self.stack.addWidget(self.menu)
+        self.stack.addWidget(self.login)
+        self.stack.addWidget(self.createacc)
+        self.stack.setCurrentWidget(self.menu)
 
-button_login.grid()
-button_newacc.grid()
-button_exit.grid()
+        self.menu.group_button.buttonClicked[int].connect(self.stack.setCurrentIndex)
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.stack)
+        layout.setAlignment(Qt.AlignCenter)
+        self.setLayout(layout)
 
 
-root.mainloop()
+class MenuWindow(QWidget):
+
+    def __init__(self):
+        super(MenuWindow, self).__init__()
+        self.setGeometry(0, 0, 250, 200)
+        box = QVBoxLayout()
+
+        self.setAutoFillBackground(True)
+        p = self.palette()
+        p.setColor(self.backgroundRole(), Qt.red)
+        self.setPalette(p)
+
+        label = QLabel('Welcome to finance tracker')
+        label.setStyleSheet('font: 24pt')
+        box.addWidget(label, alignment=Qt.AlignCenter)
+
+        login_button = QPushButton('Login')
+        new_button = QPushButton('Create a new account')
+        exit_button = QPushButton('Exit')
+        exit_button.clicked.connect(qApp.exit)
+
+        self.group_button = QButtonGroup()
+        for i, button in enumerate([login_button, new_button, exit_button]):
+            button.setStyleSheet('font: 14pt')
+            button.setFixedSize(200, 50)
+            box.addWidget(button, alignment=Qt.AlignCenter)
+            self.group_button.addButton(button)
+            self.group_button.setId(button, i+1)
+
+        self.setLayout(box)
+        self.show()
+
+
+class LoginWindow(QWidget):
+
+    def __init__(self):
+        super(LoginWindow, self).__init__()
+        self.setGeometry(0, 0, 250, 200)
+
+        self.setAutoFillBackground(True)
+        p = self.palette()
+        p.setColor(self.backgroundRole(), Qt.blue)
+        self.setPalette(p)
+
+        label = QLabel('Welcome to finance tracker')
+        box = QVBoxLayout()
+        box.addWidget(label)
+        self.setLayout(box)
+        self.show()
+
+
+class CreateAccWindow(QWidget):
+
+    def __init__(self):
+        super(CreateAccWindow, self).__init__()
+        self.setGeometry(0, 0, 250, 200)
+
+        self.setAutoFillBackground(True)
+        p = self.palette()
+        p.setColor(self.backgroundRole(), Qt.yellow)
+        self.setPalette(p)
+
+        label = QLabel('Welcome to finance tracker')
+        box = QVBoxLayout()
+        box.addWidget(label)
+        self.setLayout(box)
+        self.show()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = ApplicationWindow()
+    window.show()
+    sys.exit(app.exec())
