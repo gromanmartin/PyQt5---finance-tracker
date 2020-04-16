d4b32eedbaf7b2c7524236ad24e312f85951ece1
learninghowtocode9 <learninghowtocode9@gmail.com>
2020-04-15 22:25:28 -0600

    Phase three- create account works as intended

--- finance.py
+++ finance.py
@@ -2,6 +2,7 @@ from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 import sqlite3
+# from createacc_module import CreateAccWindow
 import sys
 
 
@@ -30,6 +31,9 @@ class ApplicationWindow(QWidget):
 
         self.menu.group_button.buttonClicked[int].connect(self.stack.setCurrentIndex)
         self.createacc.createacc_buttons.buttonClicked[int].connect(self.stack.setCurrentIndex)
+        self.createacc.resp_button_group.buttonClicked[int].connect(self.stack.setCurrentIndex)
+        self.createacc.resp_button_group.buttonClicked[int].connect(self.createacc.resp.close)
+        self.login.login_buttons.buttonClicked[int].connect(self.stack.setCurrentIndex)
 
         layout = QVBoxLayout()
         layout.addWidget(self.stack)
@@ -75,16 +79,63 @@ class LoginWindow(QWidget):
     def __init__(self):
         super(LoginWindow, self).__init__()
         self.setGeometry(0, 0, 350, 200)
-
         self.setAutoFillBackground(True)
         p = self.palette()
         p.setColor(self.backgroundRole(), Qt.lightGray)
         self.setPalette(p)
+        regex = QRegExp("[a-zA-Z1-9]+")
+        validator = QRegExpValidator(regex)
 
-        label = QLabel('Welcome to finance tracker')
-        box = QVBoxLayout()
-        box.addWidget(label)
-        self.setLayout(box)
+        label = QLabel('Login')
+        label.setAlignment(Qt.AlignCenter)
+
+        user_name_label = QLabel('Username')
+        user_name_label.setFixedWidth(50)
+        self.user_name_edit = QLineEdit()
+        self.user_name_edit.setValidator(validator)
+        self.user_name_edit.setFixedWidth(150)
+        password_label = QLabel('Password')
+        password_label.setFixedWidth(50)
+        self.password_edit = QLineEdit()
+        self.password_edit.setEchoMode(QLineEdit.Password)
+        self.password_edit.setValidator(validator)
+        self.password_edit.setFixedWidth(150)
+
+        self.wrong_label = QLabel('')
+        self.wrong_label.setFixedWidth(150)
+
+        main_grid = QVBoxLayout()
+        main_grid.addWidget(label)
+        grids = []
+        for i in range(0, 2):
+            new_grid = QHBoxLayout()
+            grids.append(new_grid)
+            main_grid.addLayout(grids[i])
+
+        form_list = [(user_name_label, self.user_name_edit),
+                     (password_label, self.password_edit)]
+        for i, pair in enumerate(form_list):
+            grids[i].addWidget(pair[0])
+            grids[i].addWidget(pair[1])
+        main_grid.addWidget(self.wrong_label, alignment=Qt.AlignHCenter)
+
+        buttons_layout = QHBoxLayout()
+        main_grid.addLayout(buttons_layout)
+        buttons_layout.addStretch(50)
+
+        self.login_button = QPushButton('Login')
+        self.back_button = QPushButton('Back')
+        self.login_buttons = QButtonGroup()
+
+        for i, button in enumerate([self.login_button, self.back_button]):
+            button.setFixedWidth(100)
+            buttons_layout.addWidget(button, alignment=Qt.AlignCenter)
+            self.login_buttons.addButton(button)
+
+        self.login_buttons.setId(self.back_button, 0)
+        buttons_layout.addStretch(50)
+
+        self.setLayout(main_grid)
         self.show()
 
 
@@ -92,7 +143,6 @@ class CreateAccWindow(QWidget):
 
     def __init__(self):
         super(CreateAccWindow, self).__init__()
-        # self.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Minimum)
         self.setAutoFillBackground(True)
         p = self.palette()
         p.setColor(self.backgroundRole(), Qt.lightGray)
@@ -162,10 +212,10 @@ class CreateAccWindow(QWidget):
 
         self.create_button = QPushButton('Create account')
         self.create_button.setEnabled(False)
-        back_button = QPushButton('Back')
+        self.back_button = QPushButton('Back')
         self.createacc_buttons = QButtonGroup()
 
-        for i, button in enumerate([self.create_button, back_button]):
+        for i, button in enumerate([self.create_button, self.back_button]):
             button.setFixedWidth(100)
             buttons_layout.addWidget(button, alignment=Qt.AlignCenter)
             self.createacc_buttons.addButton(button)
@@ -173,9 +223,23 @@ class CreateAccWindow(QWidget):
         buttons_layout.addStretch(50)
 
         self.create_button.clicked.connect(self.create_user)
-
         self.createacc_buttons.setId(self.create_button, 4)  # ID = 4
-        self.createacc_buttons.setId(back_button, 0)    # ID = 0 will move me back to main menu
+        self.createacc_buttons.setId(self.back_button, 0)    # ID = 0 will move me back to main menu
+
+        self.resp = QDialog()
+        self.resp.resize(200, 100)
+        self.resp.setWindowTitle(' ')
+        self.resp.setWindowModality(Qt.ApplicationModal)
+        self.resp_account_exist = True
+        self.resp_layout = QVBoxLayout()
+        self.resp_label = QLabel('Your account has been created. \n Click Ok to return to main menu.', self.resp)
+        self.resp_label.setAlignment(Qt.AlignCenter)
+        self.resp_button = QPushButton('Ok', self.resp)
+        self.resp_button_group = QButtonGroup()
+        self.resp_button_group_id = 0
+        self.resp_layout.addWidget(self.resp_label)
+        self.resp_layout.addWidget(self.resp_button)
+        self.resp.setLayout(self.resp_layout)
 
         self.setLayout(main_grid)
         self.setGeometry(0, 0, 350, 200)
@@ -223,11 +287,27 @@ class CreateAccWindow(QWidget):
         c = conn.cursor()
         user_name_text = self.user_name_edit.text()
         password_text = self.password_edit.text()
-        try:
-            c.execute(" INSERT INTO users VALUES (:user_name, :password)", {'user_name': user_name_text, 'password': password_text})
-            conn.commit()
-        except sqlite3.OperationalError:
-            print('lol')
+        c.execute(" SELECT username FROM users")
+        db_user_names = [un[0] for un in c.fetchall()]
+        if user_name_text not in db_user_names:
+            self.resp_button_group_id = 0
+            self.resp_button_group.addButton(self.resp_button, self.resp_button_group_id)
+            self.resp_label.setText('Your account has been created. \n Click Ok to return to main menu.')
+            c.execute(" SELECT COUNT(id) FROM users")
+            user_id = c.fetchone()[0]
+            try:
+                c.execute(" INSERT INTO users VALUES (:id, :user_name, :password)",
+                          {'id': user_id, 'user_name': user_name_text, 'password': password_text})
+                conn.commit()
+                self.resp.exec_()
+            except sqlite3.OperationalError:
+                self.resp_label.setText('Something went wrong')
+                self.resp.exec_()
+        else:
+            self.resp_button_group_id = 4
+            self.resp_button_group.addButton(self.resp_button, self.resp_button_group_id)
+            self.resp_label.setText('Choose different username')
+            self.resp.exec_()
 
 
 if __name__ == '__main__':
